09045584146fc4d5e45cbec5cd86dbe158061de3
satriahrh <satriahrh@users.noreply.github.com>
2022-04-30 17:12:42 +0000

    deployed via Gatsby Publish Action 🎩 for 322092f71235a3c188d2a286146807fb955a0511

---
+++ component---src-pages-404-js-f6f67d1193f979e60442.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmonocoque=self.webpackChunkmonocoque||[]).push([[883],{429:function(e,t,n){n.r(t);var a=n(7294),u=n(262);t.default=function(e){var t=e.data;e.location,t.site.siteMetadata.title;return a.createElement(a.Fragment,null,a.createElement(u.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f6f67d1193f979e60442.js.map
